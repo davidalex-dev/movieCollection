a4d53a180d214fe181efae99c7f76218ecc6225e
David Alex <dalexander03@student.ciputra.ac.id>
2021-10-16 12:06:12 +0700

    Initial commit. First version of movieCollection.

---
+++ app/src/main/java/com/example/moviecollection/helper/Const.java
@@ -0,0 +1,8 @@
+package com.example.moviecollection.helper;
+
+public class Const {
+
+    public static final String API_KEY = "08e27f55e2b978b2be2b8e287324171c";
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+    public static final String IMG_URL = "https://image.tmdb.org/t/p/w500/";
+}
